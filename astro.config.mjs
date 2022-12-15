5e9c53e2f045076b727bd0b990ad008ea11ff293
peteratt <pietro.attardi.pa@gmail.com>
2022-12-15 23:23:59 +0100

    changes vercel to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
